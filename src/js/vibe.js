abdcc58b0b216dd2763dc5e6bb648f31a0f2b309
Yasir326 <yasirk@nandos.co.uk>
2020-04-22 16:32:09 +0100

    first commit

---
+++ src/js/vibe.js
@@ -0,0 +1,11 @@
+const vibes = [
+    "...you are awesome!",
+    "...have a wonderful day!",
+    "...and you've got this!",
+    "...and so is this puppy! 🐶 "
+]
+
+
+let vibe = vibes[Math.floor(Math.random() * Math.floor(vibes.length))];
+
+document.querySelector(".vibe").append(vibe);
